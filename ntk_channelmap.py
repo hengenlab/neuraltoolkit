246278fab62012d22bbdcacf1650e3a2873de959
kbn-gh <kbn.git@gmail.com>
2019-01-01 23:18:17 -0600

    chan_map

--- ntk_channelmap.py
+++ ntk_channelmap.py
@@ -39,8 +39,8 @@ def find_channel_map(hstype, number_of_channels):
                 17, 21, 29, 25, 1,  5 , 13, 9,  11, 15, 23, 19, 3,  7,  31, 27]) - 1
 
     # Ecube eibless-hs64
-    if hstype == 'eibless-hs64':
-       chanMap = np.array([1, 5, 9, 13, 3, 7, 11, 15, 17, 21, 25, 29, 19, 23, 27, 31, \
+    elif hstype == 'eibless-hs64':
+       chan_map = np.array([1, 5, 9, 13, 3, 7, 11, 15, 17, 21, 25, 29, 19, 23, 27, 31, \
                33, 37, 41, 45, 35, 39, 43, 47, 49, 53, 57, 61, 51, 55, 59, 63, \
                2, 6, 10, 14, 4, 8, 12, 16, 18, 22, 26, 30, 20, 24, 28, 32, \
                34, 38, 42, 46, 36, 40, 44, 48, 50, 54, 58, 62, 52, 56, 60, 64]) - 1
@@ -52,21 +52,21 @@ def find_channel_map(hstype, number_of_channels):
 
     # KS Si probe
     elif hstype == 'Si_64_KS_chmap':
-        chanMap = np.array([7,  45, 5,  56, 4,  48, 1,  62, 9,  53, 10, 42, 14, 59, 13, 39, \
+        chan_map = np.array([7,  45, 5,  56, 4,  48, 1,  62, 9,  53, 10, 42, 14, 59, 13, 39, \
                 18, 49, 16, 36, 23, 44, 19, 33, 26, 40, 22, 30, 31, 35, 25, 27, \
                 3,  51, 2,  63, 8,  64, 6,  61, 12, 60, 11, 57, 17, 58, 15, 54, \
                 21, 55, 20, 52, 29, 50, 24, 46, 34, 43, 28, 41, 38, 47, 32, 37]) - 1
 
     # KT Si probe
     elif hstype == 'Si_64_KT_T1_K2_chmap':
-        chanMap = np.array([14, 59, 10, 42, 9,  53, 1,  62, 4,  48, 5,  56, 7,  45, 13, 39, \
+        chan_map = np.array([14, 59, 10, 42, 9,  53, 1,  62, 4,  48, 5,  56, 7,  45, 13, 39, \
                 18, 49, 16, 36, 23, 44, 19, 33, 26, 40, 22, 30, 31, 35, 25, 27, \
                 3,  51, 2,  63, 8,  64, 6,  61, 12, 60, 11, 57, 17, 58, 15, 54, \
                 21, 55, 20, 52, 29, 50, 24, 46, 34, 43, 28, 41, 38, 47, 32, 37]) - 1
 
-    # Linear probe   
+    # Linear probe
     elif hstype == 'linear':
-        chanMap = np.arange(0, number_of_channels, 1)
+        chan_map = np.arange(0, number_of_channels, 1)
 
     else:
         print("Error: Headstage type")
