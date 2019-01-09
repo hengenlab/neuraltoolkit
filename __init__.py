3e718283b6c9307aa6e10d55a84798d61667e1f5
ltilden <43013331+ltilden@users.noreply.github.com>
2019-01-08 19:32:15 -0600

    Added tools to build neuron and network objects

--- __init__.py
+++ __init__.py
@@ -4,3 +4,5 @@ from .ntk_channelmap import *
 from .ntk_plots import *
 from .load_intan_rhd_format_hlab import read_data
 from .ntk_intan import *
+from .build_neuron_backbone import *
+from .nrn_net import *
