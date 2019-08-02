33bbb5f505cc069cb9ff88ad5465b8b684e29829
kbn-gh <kbn.git@gmail.com>
2019-08-02 16:05:25 -0500

    plot raw and bandpassed data

---
+++ plot_rawandbanpassdata.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+Script to plot raw data and bandpassed data
+
+Hengen Lab
+Washington University in St. Louis
+Author: Kiran Bhaskaran-Nair
+Email: kbn.git@gmail.com
+Version:  0.1
+
+
+'''
+
+
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('Run command : conda install numpy')
+try:
+    from matplotlib import pyplot as plt
+except ImportError:
+    raise ImportError('Run command : conda install numpy')
+import neuraltoolkit as ntk
+
+# Get filename
+print("Enter filename ")
+rawfile = str(input())
+
+# Get number of channels
+print("Enter total number of probes: ")
+number_of_probes = np.int16(eval(input()))
+
+# Get number of channels
+print("Enter total number of channels : ")
+number_of_channels = np.int16(eval(input()))
+print(number_of_channels)
+
+if number_of_probes > 1:
+    hs = []
+    for i in range(number_of_probes):
+        # Get number of channels
+        print("Enter probe type (Ex. hs64) : ")
+        hstype = input()
+        print(hstype)
+        hs.append(hstype)
+else:
+    # Get number of channels
+    print("Enter probe type (Ex. hs64) : ")
+    hs = input()
+    print(hs)
+
+print("hstype ", hs)
+
+# Get number of seconds
+print("Enter total number of seconds to plot : ")
+nsec = np.int16(eval(input()))
+print(nsec)
+
+# Get nchs
+print("Enter every n channel to plot")
+print("For ex: 4, plots channels")
+print("[ 0,  4,  8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60]")
+nchs = np.int16(eval(input()))
+print(nchs)
+
+ch_list = np.arange(0, number_of_channels, nchs)
+print(ch_list)
+
+
+# get data
+tt, ddgc = ntk.load_raw_binary_gain_chmap_nsec(rawfile, number_of_channels, hs,
+                                               25000, nsec, number_of_probes)
+
+# bandpass data
+bdgc = ntk.butter_bandpass(ddgc, 500, 7500, 25000, 3)
+
+# plot
+plt.figure(1)
+# ntk.plot_data_chlist(ddgc, 0, 25000*5, l)
+
+for i in range(len(ch_list)):
+    # print(i, " ", ch_list[i])
+    ax = plt.subplot(len(ch_list), 1, i+1)
+    plt.plot(ddgc[ch_list[i], :])
+    plt.xticks([])
+    plt.yticks([])
+    # plt.box(on=None)
+
+plt.figure(2)
+# ntk.plot_data_chlist(bdgc, 0, 25000*5, l)
+for i in range(len(ch_list)):
+    # print(i, " ", ch_list[i])
+    ax = plt.subplot(len(ch_list), 1, i+1)
+    plt.plot(bdgc[ch_list[i], :])
+    plt.xticks([])
+    plt.yticks([])
+    # plt.box(on=None)
