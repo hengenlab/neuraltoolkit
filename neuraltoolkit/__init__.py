d20fb0990651f3fd02b64b1da3fffb24d2fb9bab
kbn-gh <kbn.git@gmail.com>
2022-03-28 20:49:21 -0500

    fix imports

--- neuraltoolkit/__init__.py
+++ neuraltoolkit/__init__.py
@@ -8,8 +8,6 @@ from .ntk_channelmap import *
 from .ntk_plots import *
 from .load_intan_rhd_format_hlab import read_data
 from .ntk_intan import *
-from .build_neuron_backbone import *
-from .nrn_net import *
 from .ntk_videos import *
 from .ntk_utils import *
 from .ntk_maths import *
