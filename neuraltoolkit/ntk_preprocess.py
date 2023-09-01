2d0538254b1a503741fd89705bf16d25ddf5e560
kbn-gh <kbn.git@gmail.com>
2023-09-01 16:39:56 -0500

    remove_large_noise new algo

--- neuraltoolkit/ntk_preprocess.py
+++ neuraltoolkit/ntk_preprocess.py
@@ -31,7 +31,8 @@ def remove_large_noise(ddgc_filt, max_value_to_check=3000,
         raise ValueError(f'Error: checkchans length < 1: {len(checkchans)}')
     if (len(checkchans) > 4):
         raise ValueError(f'Error: checkchans length > 4: {len(checkchans)}')
-    print(f'plotting raw data {lplot}', flush=True)
+    if lverbose:
+        print(f'plotting raw data {lplot}', flush=True)
 
     ddgc = ddgc_filt * 1
 
@@ -49,6 +50,7 @@ def remove_large_noise(ddgc_filt, max_value_to_check=3000,
     if len(checkchans) >= 1:
         edges1 = np.argwhere(np.abs(ddgc[checkchans[0], :]) >
                              max_value_to_check).flatten()
+        edges1 = np.array(edges1, dtype='int')
     elif len(checkchans) >= 2:
         edges2 = np.argwhere(np.abs(ddgc[checkchans[1], :]) >
                              max_value_to_check).flatten()
@@ -68,24 +70,41 @@ def remove_large_noise(ddgc_filt, max_value_to_check=3000,
         edges = np.unique(np.concatenate((edges1, edges2, edges3, edges4)))
     # print(f'shape edges {edges.shape}', flush=True)
 
+    edges1 = None
+    edges2 = None
+    edges3 = None
+    edges4 = None
+    del edges1
+    del edges2
+    del edges3
+    del edges4
+
     edges_all = None
-    edges_all = []
+    # edges_all = []
+    edges_all = np.array([], dtype='int')
+    last_i = -ddgc.shape[1] + 100
     for i in edges:
-        # print("iii " ,i,  " ", min(i, (i - windowval)),  " ",
-        #       min(ddgc.shape[1], (i + windowval)))
-        ddgc[:,
-             min(i, (i - windowval)): min(ddgc.shape[1], (i + windowval))] = 0
-        edges_all.extend(list(range(min(i, (i - windowval)),
-                         min(ddgc.shape[1],
-                         (i + windowval)), 1)))
+        if lverbose:
+            print(f'i {i} last_i {last_i}')
+            print(f'(i - last_i) {(i - last_i)}')
+            print(f'windowval//2 {windowval//2}')
+            print(f'(i - last_i) > windowval//2 {(i - last_i) > windowval//2}')
+        if (i - last_i) > windowval//2:
+            edges_all = np.concatenate((edges_all,
+                                       np.arange(min(i,
+                                                     (i - windowval)),
+                                                 min(ddgc.shape[1],
+                                                     (i + windowval)), 1)))
+            last_i = i
+    if lverbose:
+        print('edges_all done', flush=True)
     edges_all = np.sort(np.unique(edges_all))
-    print(f'shape edges_all {edges_all.shape}', flush=True)
+    ddgc[:, edges_all] = 0
 
     otoc = time.time()
-    print(f'Artifact removal took {otoc - otic} seconds')
-
-    # print(f'len edges {len(edges)}', flush=True)
-    print(f'len edges_all {len(edges_all)}', flush=True)
+    if lverbose:
+        print(f'Artifact removal took {otoc - otic} seconds')
+        print(f'len edges_all {len(edges_all)}', flush=True)
     if len(edges) < 1:
         lplot = 0
 
@@ -93,8 +112,9 @@ def remove_large_noise(ddgc_filt, max_value_to_check=3000,
         fig, ax = plt.subplots(nrows=3, figsize=(40, 6), sharex=True)
         fig.suptitle(f'Channel {checkchans[0]}')
         for indx, ch in enumerate(checkchans[0:1]):
-            print(f'indx {indx} ch {ch}')
-            print(f'edges {edges}', flush=True)
+            if lverbose:
+                print(f'indx {indx} ch {ch}')
+                print(f'edges {edges}', flush=True)
             ax[3*indx].plot(ddgc_filt[ch, :])
             ax[(3*indx) + 1].plot(ddgc[ch, :])
             ax[(3*indx) + 2].plot(edges_all, np.arange(len(edges_all)))
@@ -103,13 +123,15 @@ def remove_large_noise(ddgc_filt, max_value_to_check=3000,
         plt.show()
     elif lplot != 0:
         if op.exists(op.dirname(lplot)):
-            print(f'plotting raw data {lplot}', flush=True)
-            print(f'op.dirname(lplot) {op.dirname(lplot)}', flush=True)
+            if lverbose:
+                print(f'plotting raw data {lplot}', flush=True)
+                print(f'op.dirname(lplot) {op.dirname(lplot)}', flush=True)
             fig, ax = plt.subplots(nrows=3, figsize=(40, 6), sharex=True)
             fig.suptitle(f'Channel {checkchans[0]}')
             for indx, ch in enumerate(checkchans[0:1]):
-                print(f'indx {indx} ch {ch}')
-                print(f'edges {edges}', flush=True)
+                if lverbose:
+                    print(f'indx {indx} ch {ch}')
+                    print(f'edges {edges}', flush=True)
                 ax[3*indx].plot(ddgc_filt[ch, :])
                 ax[(3*indx) + 1].plot(ddgc[ch, :])
                 ax[(3*indx) + 2].plot(edges_all, np.arange(len(edges_all)))
