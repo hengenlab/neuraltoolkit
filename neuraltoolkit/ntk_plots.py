63898e00f6b0f3897f46479047b125b2a2d4dc56
kbn-gh <kbn.git@gmail.com>
2024-11-14 11:21:51 -0600

    plot_data updated

--- neuraltoolkit/ntk_plots.py
+++ neuraltoolkit/ntk_plots.py
@@ -14,20 +14,79 @@ plot_data(data, data_beg, data_end, channel=0)
 plot_data_chlist(data, data_beg, data_end, ch_list)
 '''
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def plot_data(data, data_beg, data_end, channel=0):
+
+def plot_data(data, data_beg, data_end, channel=0,
+              sampling_rate=25000,
+              file_name_with_path=None):
 
     '''
-    Plot data defaults to channel 0
-    plot_data(data, data_beg, data_end, channel=0)
-    '''
+    Plot a specified range of data from a given channel.
 
-    import matplotlib.pyplot as plt
+    plot_data(data, data_beg, data_end, channel=0,
+              sampling_rate=25000,
+              file_name_with_path=None)
+
+    Parameters:
+    -----------
+    data : np.ndarray
+        The data to plot. Can be raw, bandpassed, or LFP data.
+        Expected shape: (channels, samples).
+    data_beg : int
+        Starting sample index for the plot.
+    data_end : int
+        Ending sample index for the plot.
+    channel : int, optional
+        The channel index to plot (default is 0).
+    sampling_rate : int, optional
+        Sampling rate in Hz (default is 25000).
+    file_name_with_path : str or None, optional
+        If None, displays the plot.
+        If a file path is provided
+        (e.g., '/path/to/plot.png'), saves the plot to the file.
+
+    Returns:
+    --------
+    None
+    '''
 
     # plot
-    plt.plot(data[channel, data_beg:data_end])
-    plt.grid()
-    plt.show()
+    # plt.plot(data[channel, data_beg:data_end])
+    # plt.grid()
+    # plt.show()
+
+    color_list = ['#008080', '#ff7f50', '#a0db8e', '#b0e0e6']
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16, 4))
+
+    data = data[channel, data_beg:data_end]
+    sns.lineplot(
+        x=range(len(data)),
+        y=data,
+        ax=ax,
+        color=color_list[3],
+        # marker='o',
+        linestyle='-',
+        markersize=0.5,
+        label=f'Ch{channel}',
+        zorder=1.0
+    )
+
+    # Styling the plot
+    ax.legend(fontsize=6, loc='upper right')
+    ax.set_xlabel(f'Samples [Samples/{sampling_rate} = Seconds]', fontsize=12)
+    ax.set_ylabel('Amplitude [\u03bcV]', fontsize=12)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(axis='x', which='major', pad=50)
+    plt.tight_layout()
+    if file_name_with_path is None:
+        plt.show()
+    else:
+        plt.savefig(file_name_with_path)
 
 
 def plot_data_chlist(data, data_beg, data_end, ch_list):
