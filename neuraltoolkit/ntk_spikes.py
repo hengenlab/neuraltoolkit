2c40930d558b2e4c7df6ad4a3cc37bc525587901
kbn-gh <kbn.git@gmail.com>
2024-11-27 08:37:20 -0600

    updated cell_isi_hist

--- neuraltoolkit/ntk_spikes.py
+++ neuraltoolkit/ntk_spikes.py
@@ -49,7 +49,7 @@ def make_spikematrix(spiketimes_ms, start, end, binsz, binarize=False):
     return spike_matrix
 
 
-def cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
+def cell_isi_hist(spiketimes_s, start=False, end=False, isi_thresh=0.1,
                   nbins=101, lplot=1):
 
     '''
@@ -63,14 +63,14 @@ def cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
     This function will plot the bar histogram of that distribution
     and calculate the percentage of ISIs that fall under 2 msec.
 
-    cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
+    cell_isi_hist(spiketimes_s, start=False, end=False, isi_thresh=0.1,
                   nbins=101, lplot=1)
 
     Parameters
     ----------
-    time_s : time in seconds
+    spiketimes_s : Spike times for each neuron (in seconds)
     start : Start time (default False uses 0)
-    end : End time (default False uses np.max(time_s))
+    end : End time (default False uses np.max(spiketimes_s))
     isi_thresh : isi threshold (default 0.1)
     nbins : Number of bins (default 101)
     lplot : To plot or not (default lplot=1, plot isi)
@@ -94,7 +94,7 @@ def cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
 
     Examples
     --------
-    cell_isi_hist(time_s, start=False, end=False,
+    cell_isi_hist(spiketimes_s, start=False, end=False,
                   isi_thresh=0.1, nbins=101, lplot=1)
 
     '''
@@ -102,11 +102,12 @@ def cell_isi_hist(time_s, start=False, end=False, isi_thresh=0.1,
     if start is False:
         start = 0
     if end is False:
-        end = np.max(time_s)
+        end = np.max(spiketimes_s)
 
     # Calulate isi
-    idx_l = np.where(np.logical_and(time_s >= start, time_s <= end))[0]
-    ISI = np.diff(time_s[idx_l])
+    idx_l = \
+        np.where(np.logical_and(spiketimes_s >= start, spiketimes_s <= end))[0]
+    ISI = np.diff(spiketimes_s[idx_l])
 
     # plot histogram and calculate contamination
     edges = np.linspace(0, isi_thresh, nbins)
