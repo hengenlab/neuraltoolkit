008749715a40407cef66631b2c8d8b6d4bb40bb5
kbn-gh <kbn.git@gmail.com>
2025-12-04 13:27:14 -0600

    updated tag

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ except Exception as e:
 
 setup(
    name='neuraltoolkit',
-   version='0.4.0',
+   version='0.4.2',
    description='A powerful and fast set of tools for loading data, filtering,\
                 processing, working with data formats, and basic utilities for\
                 electrophysiology and behavioral data.',
